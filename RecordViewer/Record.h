92ca0892e5072e826caa00fa43caf8f9292d7dbc
Tomasz Ostrowski <tomasz.o.ostrowski@gmail.com>
2023-05-23 19:33:32 +0200

    Separate record list entry

---
+++ RecordViewer/Record.h
@@ -0,0 +1,29 @@
+//---------------------------------------------------------------------------
+
+#ifndef RecordH
+#define RecordH
+//---------------------------------------------------------------------------
+
+#include <System.hpp>
+
+struct S_RECORD
+{
+	int id;		///< important for records_filtered only; mapping records_filtered to records
+	AnsiString asFilename;
+	enum DIR {
+		DIR_UNKNOWN = 0,
+		DIR_IN,
+		DIR_OUT
+	} dir;
+	AnsiString asDateTime;
+	AnsiString asNumber;
+	AnsiString asDescription;
+	int size;
+	S_RECORD(void):
+		id(-1),
+		dir(DIR_UNKNOWN),
+		size(0)
+	{}
+};
+
+#endif
